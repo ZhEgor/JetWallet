3c632750510b53e199fc5a54b88ed829de09c2ef
zhiroke\Egor Zhir <zhirokegor@gmail.com>
2022-12-26 16:31:59 +0200

    custom task test

--- buildSrc/src/main/kotlin/plugins/KotlinAndroidConfigurationPlugin.kt
+++ buildSrc/src/main/kotlin/plugins/KotlinAndroidConfigurationPlugin.kt
@@ -1,14 +1,27 @@
 package plugins
 
-import com.android.build.api.dsl.ApplicationExtension
+import Configs
 import com.android.build.api.dsl.CommonExtension
 import com.android.build.gradle.LibraryExtension
 import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.tasks.TaskAction
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.file.FileSystemOperations
+import org.gradle.api.file.FileType
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.*
+import org.gradle.api.tasks.options.Option
 import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.register
+import org.gradle.kotlin.dsl.submit
+import org.gradle.work.ChangeType
+import org.gradle.work.Incremental
+import org.gradle.work.InputChanges
+import org.gradle.workers.WorkAction
+import org.gradle.workers.WorkParameters
+import org.gradle.workers.WorkerExecutor
+import javax.inject.Inject
 
 class KotlinAndroidConfigurationPlugin : Plugin<Project> { // TODO: Finalize
 
@@ -36,5 +49,100 @@ abstract class HelloTask : DefaultTask() {
     @TaskAction
     fun taskAction() {
         println("Hello from " + project.parent?.name)
+    }
+}
+
+abstract class IncrementalReverseTask : DefaultTask() {
+    @get:Incremental
+    @get:PathSensitive(PathSensitivity.NAME_ONLY)
+    @get:InputDirectory
+    abstract val inputDir: DirectoryProperty
+
+    @get:OutputDirectory
+    abstract val outputDir: DirectoryProperty
+
+    @get:Input
+    abstract val inputProperty: Property<String>
+
+    @TaskAction
+    fun execute(inputChanges: InputChanges) {
+        println(
+            if (inputChanges.isIncremental) "Executing incrementally"
+            else "Executing non-incrementally"
+        )
+
+        inputChanges.getFileChanges(inputDir).forEach { change ->
+            if (change.fileType == FileType.DIRECTORY) return@forEach
+
+            println("${change.changeType}: ${change.normalizedPath}")
+            val targetFile = outputDir.file(change.normalizedPath).get().asFile
+            if (change.changeType == ChangeType.REMOVED) {
+                targetFile.delete()
+            } else {
+                targetFile.writeText(change.file.readText().reversed())
+            }
+        }
+    }
+}
+
+abstract class UrlVerify : DefaultTask() {
+    @get:Input
+    @set:Option(option = "url", description = "Configures the URL to be verified.")
+    var url: String? = null
+
+    @TaskAction
+    fun verify() {
+        logger.quiet("Verifying URL '{}'", url)
+
+        // verify URL by making a HTTP call
+    }
+}
+
+// The parameters for a single unit of work
+interface ReverseParameters : WorkParameters {
+    val fileToReverse : RegularFileProperty
+    val destinationDir : DirectoryProperty
+}
+
+abstract class ReverseFile @Inject constructor(val fileSystemOperations: FileSystemOperations) : WorkAction<ReverseParameters> {
+    override fun execute() {
+        fileSystemOperations.copy {
+            from(parameters.fileToReverse)
+            from(parameters.destinationDir)
+            filter { line -> line.reversed() }
+        }
+    }
+}
+
+abstract class ReverseFiles @Inject constructor(private val workerExecutor: WorkerExecutor) : SourceTask() {
+
+    @get:OutputDirectory
+    abstract val outputDir: DirectoryProperty
+
+    @TaskAction
+    fun reverseFiles() {
+        /*// Create a WorkQueue with process isolation
+        val workQueue = workerExecutor.processIsolation() {
+            // Configure the options for the forked process
+            forkOptions {
+                maxHeapSize = "512m"
+                systemProperty("org.gradle.sample.showFileSize", "true")
+            }
+        }
+        */
+
+        // Create a WorkQueue to submit work items
+        val workQueue = workerExecutor.noIsolation()
+
+        // Create and submit a unit of work for each file
+        source.forEach { file ->
+            workQueue.submit(ReverseFile::class) {
+                fileToReverse.set(file)
+                destinationDir.set(outputDir)
+            }
+        }
+        // Wait for all asynchronous work submitted to this queue to complete before continuing
+        workQueue.await()
+        logger.lifecycle("Created ${outputDir.get().asFile.listFiles()?.size} reversed files in ${outputDir.get().asFile.listFiles()?.size}")
     }
 }
--- core/theme/build.gradle.kts
+++ core/theme/build.gradle.kts
@@ -1,3 +1,9 @@
+
+import plugins.HelloTask
+import plugins.IncrementalReverseTask
+import plugins.ReverseFiles
+import plugins.UrlVerify
+
 plugins {
     id("com.android.library")
     kotlin("android")
@@ -15,6 +21,9 @@ android {
     }
 
     buildTypes {
+        getByName("debug") {
+            isMinifyEnabled = false
+        }
         getByName("release") {
             isMinifyEnabled = false
             proguardFiles(
@@ -44,4 +53,35 @@ dependencies {
     implementation(Libs.AndroidX.appCompat)
     implementation(Libs.Compose.ui)
     implementation(Libs.Compose.material3)
+}
+
+tasks.register<HelloTask>(name = "hello") {
+    shouldRunAfter("build")
+}.get()
+
+tasks.register<IncrementalReverseTask>(name = "incrementalReverse") {
+    inputDir.set(file("inputs"))
+    outputDir.set(file("$buildDir/outputs"))
+    inputProperty.set(project.findProperty("taskInputProperty") as String? ?: "original")
+}
+
+tasks.register("updateInputs") {
+    outputs.dir("inputs")
+    doLast {
+        file("inputs/1.txt").writeText("Changed content for existing file 1.")
+        file("inputs/4.txt").writeText("Content for new file 4.")
+    }
+}
+
+tasks.register("removeOutput") {
+    outputs.dir("$buildDir/outputs")
+    doLast {
+        file("$buildDir/outputs/1.txt").delete()
+    }
+}
+
+tasks.register<UrlVerify>("verifyUrl")
+
+tasks.register<ReverseFiles>("reverseFiles") {
+    outputs.dir("$buildDir/outputs")
 }
