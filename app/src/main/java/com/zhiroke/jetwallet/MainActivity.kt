7a76fee93267f98fd9a6a31052cf525b1a042a7c
zhiroke\Egor Zhir <zhirokegor@gmail.com>
2023-06-05 09:15:37 +0300

    :recycle:: made LayoutParams.FLAG_SECURE only for release

--- app/src/main/java/com/zhiroke/jetwallet/MainActivity.kt
+++ app/src/main/java/com/zhiroke/jetwallet/MainActivity.kt
@@ -13,7 +13,9 @@ class MainActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
+        if (!BuildConfig.DEBUG) {
+            window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
+        }
         setContent(content = { JetWallet() })
         attachBiometricAuthManager(activity = this)
     }
